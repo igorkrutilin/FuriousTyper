15b09b82dc3e75a3db152d7e0d49be0a3de676e7
igorkrutilin <irogkrutilin03@gmail.com>
2018-11-04 23:39:15 +0200

    player now earns coins for typing words

--- game.py
+++ game.py
@@ -1,29 +1,56 @@
 import pygame
-import random
+from random import randint
+import sqlite3
 
 pygame.init()
+
+conn = sqlite3.connect("database.db")
+try:
+    conn.execute("create table coins (number int)")
+    conn.execute("insert into coins (number) values (0)")
+except:
+    pass
+cursor = conn.cursor()
 
 window_size = [500, 500]
 
 window = pygame.display.set_mode(window_size)
 
-def update(typed_word, word_to_type):
+def get_coin_number(cursor):
+    cursor.execute("select Number from Coins")
+    result = cursor.fetchall()
+    coins = result[0][0]
+
+    return coins
+
+def change_coin_number(coins, operation, number, conn):
+    if operation == "+":
+        coins += number
+    conn.execute("delete from coins")
+    conn.execute("insert into coins (number) values (" + str(coins) + ")")
+    conn.commit()
+
+def update(typed_word, word_to_type, coins):
     window.fill((255, 255, 255))
 
     font = pygame.font.SysFont("serif", 36)
 
-    rendered_typed_word = font.render(typed_word, False, (0, 0, 0))
-    window.blit(rendered_typed_word, (100, 100))
-
     rendered_word_to_type = font.render(word_to_type, False, (0, 0, 0))
     window.blit(rendered_word_to_type, (100, 50))
 
+    rendered_typed_word = font.render(typed_word, False, (0, 0, 0))
+    window.blit(rendered_typed_word, (100, 100))
+
+    rendered_coins = font.render(str(coins), False, (0, 0, 0))
+    window.blit(rendered_coins, (450, 20))
+
     pygame.display.update()
 
-def generate_word_to_type():
+def generate_word():
     word_list = open("words.txt", "r")
     words = word_list.readlines()
-    num = random.randint(0, len(words) - 1)
+    word_list.close()
+    num = randint(0, len(words) - 1)
     return words[num][:-1]
 
 typed_word = ""
@@ -42,11 +69,14 @@ while run:
                 typed_word = typed_word[:-1]
 
     if typed:
-        word_to_type = generate_word_to_type()
+        word_to_type = generate_word()
         typed = False
+
+    coins = get_coin_number(cursor)
 
     if typed_word == word_to_type:
         typed_word = ""
-        typed = True
+        coins = change_coin_number(coins, "+", 1, conn)
+        word_to_type = generate_word()
 
-    update(typed_word, word_to_type)
+    update(typed_word, word_to_type, coins)
