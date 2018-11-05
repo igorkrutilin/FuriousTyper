5b358c0007c6cace7caefc85073d262ffa11db74
igorkrutilin <irogkrutilin03@gmail.com>
2018-11-05 21:18:34 +0200

    coin animation added

--- game.py
+++ game.py
@@ -16,6 +16,13 @@ window_size = [500, 500]
 
 window = pygame.display.set_mode(window_size)
 
+anim_location = "./sprites/coin/"
+animation = []
+for i in range(9):
+    sprite = pygame.image.load(anim_location + "goldCoin" + str(i+1) + ".png")
+    sprite = pygame.transform.scale(sprite, (50, 50))
+    animation.append(sprite)
+
 def get_coin_number(cursor):
     cursor.execute("select Number from Coins")
     result = cursor.fetchall()
@@ -30,7 +37,14 @@ def change_coin_number(coins, operation, number, conn):
     conn.execute("insert into coins (number) values (" + str(coins) + ")")
     conn.commit()
 
-def update(typed_word, word_to_type, coins):
+def generate_word():
+    word_list = open("words.txt", "r")
+    words = word_list.readlines()
+    word_list.close()
+    num = randint(0, len(words) - 1)
+    return words[num][:-1]
+
+def update(typed_word, word_to_type, coins, anim_num):
     window.fill((255, 255, 255))
 
     font = pygame.font.SysFont("serif", 36)
@@ -42,22 +56,24 @@ def update(typed_word, word_to_type, coins):
     window.blit(rendered_typed_word, (100, 100))
 
     rendered_coins = font.render(str(coins), False, (0, 0, 0))
-    window.blit(rendered_coins, (450, 20))
+    rendered_coins_rect = rendered_coins.get_rect()
+    rendered_coins_rect.right = 450
+    rendered_coins_rect.top = 5
+    window.blit(rendered_coins, rendered_coins_rect)
+
+    window.blit(animation[anim_num], (window_size[1] - 50, 0))
 
     pygame.display.update()
-
-def generate_word():
-    word_list = open("words.txt", "r")
-    words = word_list.readlines()
-    word_list.close()
-    num = randint(0, len(words) - 1)
-    return words[num][:-1]
 
 typed_word = ""
 typed = True
 run = True
+anim_num = 0
+clock = pygame.time.Clock()
 
 while run:
+    clock.tick(10)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -79,4 +95,9 @@ while run:
         coins = change_coin_number(coins, "+", 1, conn)
         word_to_type = generate_word()
 
-    update(typed_word, word_to_type, coins)
+    if anim_num < 8:
+        anim_num += 1
+    else:
+        anim_num = 0
+
+    update(typed_word, word_to_type, coins, anim_num)
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # FuriousTyper
 
 999 of 1000 words in words.txt file are taken from https://www.ef.com/english-resources/english-vocabulary/top-1000-words/
+
+Coin sprites are taken from https://opengameart.org/content/spinning-gold-coin
